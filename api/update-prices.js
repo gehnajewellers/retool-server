c9ec07658d1915b8a31a941eb05230d2d30f45dc
gehnajewellers <info@gehnajewellers.com>
2025-10-16 17:20:04 +0530

    fix/preflight

--- api/update-prices.js
+++ api/update-prices.js
@@ -92,7 +92,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Preflight request
+    return res.status(200).end(); 
   }
 
   if (req.method !== "POST") {
